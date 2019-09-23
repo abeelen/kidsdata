1a8ad77e13e8eb606b4684c7d8d947bf7ce125d6
Alexandre Beelen <alexandre.beelen@lam.fr>
2019-09-23 15:51:15 +0200

    Doc, blacked

--- src/db.py
+++ src/db.py
@@ -1,59 +1,74 @@
+import os
 import logging
-logging.basicConfig(level=logging.INFO)
 
+logging.basicConfig(level=logging.INFO)
 from pathlib import Path
 from itertools import chain
-from src.kids_data import KissRawData
 from datetime import datetime
-from astropy.table import Table, join # for now
+from functools import wraps
+
+from astropy.table import Table, join  # for now
 from astropy.utils.console import ProgressBar
 
-BASE_DIRS = [Path('/data/KISS/Raw/nika2c-data3/KISS'), ]
+from src.kids_data import KissRawData
+
+BASE_DIRS = [Path(os.getenv("KISS_DATA", "/data/KISS/Raw/nika2c-data3/KISS"))]
 DATABASE_SCAN = None
 DATABASE_PARAM = None
 
 
 def fill_database(dirs=None):
+    """Fill the database with the filenames.
 
+    Parameters
+    ----------
+    dirs : list
+        list of directories to scan
+    """
     global DATABASE_SCAN
 
     if dirs is None:
         dirs = BASE_DIRS
 
-    filenames = chain(*[path.glob('X*_*_S????_*_*') for path in dirs])
+    filenames = chain(*[Path(path).glob("X*_*_S????_*_*") for path in dirs])
 
     data_rows = []
     for filename in filenames:
-        date, hour, scan, source, obsmode = filename.name[1:].split('_')
-        dtime = datetime.strptime(' '.join([date, hour]), '%Y%m%d %H%M')
-        scan =  int(scan[1:])
+        date, hour, scan, source, obsmode = filename.name[1:].split("_")
+        dtime = datetime.strptime(" ".join([date, hour]), "%Y%m%d %H%M")
+        scan = int(scan[1:])
         data_rows.append((filename.as_posix(), dtime, scan, source, obsmode))
 
-    DATABASE_SCAN = Table(names=['filename', 'date', 'scan', 'source', 'obsmode'], rows=data_rows)
-    DATABASE_SCAN.sort('date')
+    DATABASE_SCAN = Table(names=["filename", "date", "scan", "source", "obsmode"], rows=data_rows)
+    DATABASE_SCAN.sort("date")
+
 
 def auto_fill(func):
+    @wraps(func)
     def wrapper(*args, **kwargs):
         if DATABASE_SCAN is None:
             fill_database()
         return func(*args, **kwargs)
+
     return wrapper
+
 
 @auto_fill
 def extend_database():
-
+    """Read the header of each file and construct the parameter database."""
     global DATABASE_SCAN, DATABASE_PARAM
+
     data_rows = []
     param_rows = {}
-    for filename in ProgressBar(DATABASE_SCAN['filename']):
+    for filename in ProgressBar(DATABASE_SCAN["filename"]):
         kd = KissRawData(filename)
-        hash_param =  hash(str(kd.param_c))
-        data_row = {'filename': filename, 'param_id': hash_param}
-        param_row = {'param_id': hash_param}
+        hash_param = hash(str(kd.param_c))
+        data_row = {"filename": filename, "param_id": hash_param}
+        param_row = {"param_id": hash_param}
         param_row.update(kd.param_c)
         data_rows.append(data_row)
         param_rows[hash_param] = param_row
-        del(kd)
+        del kd
 
     param_rows = [*param_rows.values()]
 
@@ -71,23 +86,49 @@ def extend_database():
 
 @auto_fill
 def get_scan(scan=None):
+    """Get filename of corresponding scan number.
 
-    mask = DATABASE_SCAN['scan'] == scan
-    return DATABASE_SCAN[mask]['filename'].data[0]
+    Parameters
+    ----------
+    scan : int
+        the scan number to retrieve
+
+    Returns
+    -------
+    filename : str
+       the full path of the file
+    """
+    mask = DATABASE_SCAN["scan"] == scan
+    return DATABASE_SCAN[mask]["filename"].data[0]
+
 
 @auto_fill
 def list_scan(**kwargs):
+    """List (with filtering) all scans in the database.
 
+    Notes
+    -----
+    One can filter on any key of the database by giving kwargs, for example
+
+    >>> list_scan(source="Moon")
+
+    will display all the scan on the moon. One can also use the `__gt` and
+    `__lt` postfix to the keyword to filter greather_tan and lower_than values :
+
+    >>> list_scan(scan__gt=400)
+
+    will return all the scan greather than 400
+    """
     _database = DATABASE_SCAN
 
     # Filtering on all possible key from table
     for key in kwargs.keys():
-        if key in DATABASE_SCAN.keys():
-            if '_gt' in key:
-                _database = _database[_database[key.split('_')[0]] > kwargs[key]]
-            elif '_lt' in key:
-                _database = _database[_database[key.split('_')[0]] < kwargs[key]]
+        if key.split("__")[0] in DATABASE_SCAN.keys():
+            if "__gt" in key:
+                _database = _database[_database[key.split("__")[0]] > kwargs[key]]
+            elif "__lt" in key:
+                _database = _database[_database[key.split("__")[0]] < kwargs[key]]
             else:
                 _database = _database[_database[key] == kwargs[key]]
 
-    print(_database[['date', 'scan', 'source', 'obsmode']])
+    print(_database[["date", "scan", "source", "obsmode"]])
