15ae7fe654ed7c65c51d66d8824404d7f9f66233
Grégoire Duvauchelle <gregoire.duvauchelle@lam.fr>
2021-02-22 19:13:01 +0100

    Fix association table again

--- kidsdata/database/models/scan_x_product.py
+++ kidsdata/database/models/scan_x_product.py
@@ -1,6 +1,14 @@
 from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy.ext.declarative.base import declared_attr
 
-scan_x_product_base = (
-    Column("scan_id", Integer, ForeignKey("scan.id"), primary_key=True),
-    Column("product_id", Integer, ForeignKey("product.id"), primary_key=True),
-)
+
+class ScanXProductBase:
+    __tablename__ = "scan_x_product"
+
+    @declared_attr
+    def scan_id(self):
+        return Column(Integer, ForeignKey("scan.id"), primary_key=True)
+
+    @declared_attr
+    def product_id(self):
+        return Column(Integer, ForeignKey("product.id"), primary_key=True)
