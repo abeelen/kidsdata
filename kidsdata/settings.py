d50d8d6a08630ec1b8044936f9be142072741305
Grégoire Duvauchelle <gregoire.duvauchelle@lam.fr>
2021-02-23 15:55:31 +0100

    Fix database default location

--- kidsdata/settings.py
+++ kidsdata/settings.py
@@ -3,7 +3,7 @@ from decouple import config
 
 # only used in default session of database.api
 DB_DIR = config("DB_DIR",  default=".", cast=Path)
-DB_URI = config("DB_URI", default="sqlite:///" + str(DB_DIR / "kids_data.db"))
+DB_URI = config("DB_URI", default="sqlite:///" + str(DB_DIR.resolve() / "kids_data.db"))
 BASE_DIRS = [Path(directory) for directory in config("DATA_DIR", default="/data/KISS/Raw/nika2c-data3/KISS").split(";")]
 
 # other settings
