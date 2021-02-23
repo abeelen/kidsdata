98a86b2dd6dc1b4b679bdc107e3cbc2fa448e715
Grégoire Duvauchelle <gregoire.duvauchelle@lam.fr>
2021-02-23 14:44:45 +0100

    Add entrypoint to alembic environment

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     packages=find_packages(exclude=['docs', 'tests']),
     entry_points={
         'console_scripts': [
-            # 'command = some.module:some_function',
+            'kidsdata-migrate = kidsdata.database.migrate_entrypoint:call_alembic',
         ],
     },
     include_package_data=True,
