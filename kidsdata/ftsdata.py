f696c51aa4cba82fae667751e763e78c19d87e50
Alexandre Beelen <alexandre.beelen@lam.fr>
2021-02-24 11:21:02 +0100

    feat(FTSData) Add from_array constructor. Allow for arbitrary dimension objects (1D/2D/3D/...) cubes in FTSData objects, first axes must be the opd/freq axis

--- kidsdata/ftsdata.py
+++ kidsdata/ftsdata.py
@@ -7,6 +7,7 @@ from scipy.signal import fftconvolve, medfilt
 import astropy.units as u
 import astropy.constants as cst
 from astropy.io import fits, registry
+from astropy.wcs import WCS
 from astropy.nddata import NDDataArray, StdDevUncertainty, InverseVariance
 from astropy.nddata.ccddata import _known_uncertainties
 from astropy.nddata.ccddata import _unc_name_to_cls, _unc_cls_to_name, _uncertainty_unit_equivalent_to_parent
@@ -47,39 +48,104 @@ def forman(M):
 
 
 class FTSData(NDDataArray):
-    """Class to handle OPD or spectral cubes."""
+    """Class to handle OPD or spectral FTS cubes.
+
+    Parameters
+    ----------
+    data : `~numpy.ndarray` or `FTSData`
+        The actual data contained in this `FTSData` object. Not that this
+        will always be copies by *reference* , so you should make copy
+        the ``data`` before passing it in if that's the  desired behavior.
+
+    uncertainty : `~astropy.nddata.NDUncertainty`, optional
+        Uncertainties on the data.
+
+    mask : `~numpy.ndarray`-like, optional
+        Mask for the data, given as a boolean Numpy array or any object that
+        can be converted to a boolean Numpy array with a shape
+        matching that of the data. The values must be ``False`` where
+        the data is *valid* and ``True`` when it is not (like Numpy
+        masked arrays). If ``data`` is a numpy masked array, providing
+        ``mask`` here will causes the mask from the masked array to be
+        ignored.
+
+    hits : `~numpy.ndarray`-like, optional
+        Hit map for the data, given as a int Numpy array or any object that
+        can be converted to a int Numpy array with a shape
+        matching that of the data.
+
+    flags : `~numpy.ndarray`-like or `~astropy.nddata.FlagCollection`, optional
+        Flags giving information about each pixel. These can be specified
+        either as a Numpy array of any type (or an object which can be converted
+        to a Numpy array) with a shape matching that of the
+        data, or as a `~astropy.nddata.FlagCollection` instance which has a
+        shape matching that of the data.
+
+    wcs : `~astropy.wcs.WCS`, optional
+        WCS-object containing the world coordinate system for the data.
+
+    meta : `dict`-like object, optional
+        Metadata for this object.  "Metadata" here means all information that
+        is included with this object but not part of any other attribute
+        of this particular object.  e.g., creation date, unique identifier,
+        simulation parameters, exposure time, telescope name, etc.
+
+    unit : `~astropy.units.UnitBase` instance or str, optional
+        The units of the data.
+
+    """
+
+    __opd_idx = None
+    __freq_idx = None
+    hits = None
 
     def __init__(self, *args, hits=None, **kwargs):
+
         # Initialize with the parent...
         super().__init__(*args, **kwargs)
-        self.hits = hits
-        self._opd_idx = None
 
+        # Additionnal data
+        if hits is not None:
+            self.hits = np.array(hits).astype(int)
+
+        # Set Internal indexes on the wcs object
         if self.wcs is not None:
             opd_idx = np.argwhere("opd" == np.char.lower(self.wcs.wcs.ctype)).squeeze()
-            self._opd_idx = opd_idx.item() if opd_idx.size == 1 else None
+            self.__opd_idx = opd_idx.item() if opd_idx.size == 1 else None
+
+            freq_idx = np.argwhere("freq" == np.char.lower(self.wcs.wcs.ctype)).squeeze()
+            self.__freq_idx = freq_idx.item() if freq_idx.size == 1 else None
 
     @property
-    def _is_opd(self):
-        return self._opd_idx is not None
+    def __is_opd(self):
+        return self.__opd_idx is not None
+
+    @property
+    def __is_freq(self):
+        return self.__freq_idx is not None
 
     @property
     def opd_axis(self):
-        if self._is_opd:
-            return self.wcs.sub([self._opd_idx + 1]).pixel_to_world(np.arange(self.shape[0]))
+        if self.__is_opd:
+            return self.wcs.sub([self.__opd_idx + 1]).pixel_to_world(np.arange(self.shape[0]))
+
+    @property
+    def spectral_axis(self):
+        if self.__is_freq:
+            return self.wcs.sub([self.__freq_idx + 1]).pixel_to_world(np.arange(self.shape[0]))
 
     @property
     def _is_doublesided(self):
-        """Test if the cube is doublesided, enforce positive increments."""
-        return (np.sum(self.wcs.sub([self._opd_idx + 1]).all_pix2world([0, self.shape[0] - 1], 0)) == 0) & (
-            self.wcs.wcs.cdelt[self._opd_idx] > 0
+        """Return True is the cube is double sided, also enforce positive increments."""
+        return (np.sum(self.wcs.sub([self.__opd_idx + 1]).all_pix2world([0, self.shape[0] - 1], 0)) == 0) & (
+            self.wcs.wcs.cdelt[self.__opd_idx] > 0
         )
 
     @property
     def _is_onesided(self):
-        """Test if the cube is onesided, enforce positive increments."""
-        return (np.sum(self.wcs.sub([self._opd_idx + 1]).all_pix2world(0, 0)) == 0) & (
-            self.wcs.wcs.cdelt[self._opd_idx] > 0
+        """Return True is the cube is one sided, also enforce positive increments."""
+        return (np.sum(self.wcs.sub([self.__opd_idx + 1]).all_pix2world(0, 0)) == 0) & (
+            self.wcs.wcs.cdelt[self.__opd_idx] > 0
         )
 
     # from CCDData
@@ -105,9 +171,9 @@ class FTSData(NDDataArray):
             A doublesided interferograms cube
         """
 
-        assert self._is_opd, "Intput should be OPD cube"
+        assert self.__is_opd, "Intput should be OPD cube"
 
-        opd_wcs = self.wcs.sub([self._opd_idx + 1])
+        opd_wcs = self.wcs.sub([self.__opd_idx + 1])
         opds = opd_wcs.all_pix2world(np.arange(self.data.shape[0]), 0)[0]
 
         _maxopd = np.min([-opds.min(), opds.max()])
@@ -118,7 +184,7 @@ class FTSData(NDDataArray):
         _slice = slice(*slice_idx)
 
         wcs = deepcopy(self.wcs)
-        wcs.wcs.crpix[self._opd_idx] -= _slice.start
+        wcs.wcs.crpix[self.__opd_idx] -= _slice.start
 
         meta = deepcopy(self.meta)
         meta["HISTORY"] = "extract_doublesided"
@@ -137,9 +203,9 @@ class FTSData(NDDataArray):
         output : FTSData
             A onesided interferograms cube
         """
-        zpd_idx = self.wcs.sub([self._opd_idx + 1]).world_to_pixel(0 * self.wcs.wcs.cunit[self._opd_idx]).astype(int)
+        zpd_idx = self.wcs.sub([self.__opd_idx + 1]).world_to_pixel(0 * self.wcs.wcs.cunit[self.__opd_idx]).astype(int)
 
-        extrema_opd = np.abs(self.wcs.sub([self._opd_idx + 1]).pixel_to_world([0, self.shape[0] - 1]))
+        extrema_opd = np.abs(self.wcs.sub([self.__opd_idx + 1]).pixel_to_world([0, self.shape[0] - 1]))
 
         if extrema_opd[1] >= extrema_opd[0]:
             # Positive single sided : longer right hand side...
@@ -154,19 +220,21 @@ class FTSData(NDDataArray):
             os_slice = slice(0, self.data.shape[0] - zpd_idx)
             db_slice = slice(zpd_idx, None)
 
+        # TODO: self.mask ??
         # Extract the longest part
         onesided_itg = self.data[extract_slice].copy()
-        onesided_hits = self.hits[extract_slice].copy()
+        onesided_hits = self.hits[extract_slice].copy() if self.hits is not None else None
 
         # Take the mean with the other half on the double sided part
         onesided_itg[os_slice] += self.data[db_slice]
         onesided_itg[os_slice] /= 2
 
-        onesided_hits[os_slice] += self.hits[db_slice]
-        onesided_hits[os_slice] /= 2
+        if onesided_hits is not None:
+            onesided_hits[os_slice] += self.hits[db_slice]
+            onesided_hits[os_slice] /= 2
 
         wcs = deepcopy(self.wcs)
-        wcs.wcs.crpix[self._opd_idx] = 1
+        wcs.wcs.crpix[self.__opd_idx] = 1
 
         output = FTSData(onesided_itg, wcs=wcs, meta=self.meta, hits=onesided_hits)
         return output
@@ -194,11 +262,11 @@ class FTSData(NDDataArray):
         ----------
         .. [1] https://docs.scipy.org/doc/numpy/reference/routines.window.html
         """
-        assert self._is_opd, "Intput should be OPD cube"
+        assert self.__is_opd, "Intput should be OPD cube"
         assert self._is_doublesided, "Not a doublesided interferogram cube"
 
-        cdelt_opd = self.wcs.wcs.cdelt[self._opd_idx]
-        cunit_opd = u.Unit(self.wcs.wcs.cunit[self._opd_idx])
+        cdelt_opd = self.wcs.wcs.cdelt[self.__opd_idx]
+        cunit_opd = u.Unit(self.wcs.wcs.cunit[self.__opd_idx])
         naxis_opd = self.shape[0]
         # freq = np.fft.fftfreq(naxis_opd, d=cdelt_opd * cunit_opd) * cst.c
 
@@ -218,12 +286,12 @@ class FTSData(NDDataArray):
 
         # Build new wcs
         wcs = deepcopy(self.wcs)
-        wcs.wcs.ctype[self._opd_idx] = "FREQ"
-        wcs.wcs.cunit[self._opd_idx] = "Hz"
+        wcs.wcs.ctype[self.__opd_idx] = "FREQ"
+        wcs.wcs.cunit[self.__opd_idx] = "Hz"
         # TODO: (cst.c / (cdelt_opd * cunit_opd) / (naxis_opd-1)).to(u.Hz).value give the 1/2L resolution, but fails in the tests
-        wcs.wcs.cdelt[self._opd_idx] = (cst.c / (cdelt_opd * cunit_opd) / naxis_opd).to(u.Hz).value
-        wcs.wcs.crpix[self._opd_idx] = (naxis_opd - 1) / 2 + 1
-        wcs.wcs.crval[self._opd_idx] = 0
+        wcs.wcs.cdelt[self.__opd_idx] = (cst.c / (cdelt_opd * cunit_opd) / naxis_opd).to(u.Hz).value
+        wcs.wcs.crpix[self.__opd_idx] = (naxis_opd - 1) / 2 + 1
+        wcs.wcs.crval[self.__opd_idx] = 0
 
         # TODO: Estimate uncertainty/hits
         output = FTSData(spectra, meta=self.meta, wcs=wcs)
@@ -250,11 +318,11 @@ class FTSData(NDDataArray):
 
         .. [1] https://docs.scipy.org/doc/numpy/reference/routines.window.html
         """
-        assert self._is_opd, "Intput should be OPD cube"
+        assert self.__is_opd, "Intput should be OPD cube"
         assert self._is_onesided, "Not a one sided interferogram cube"
 
-        cdelt_opd = self.wcs.wcs.cdelt[self._opd_idx]
-        cunit_opd = u.Unit(self.wcs.wcs.cunit[self._opd_idx])
+        cdelt_opd = self.wcs.wcs.cdelt[self.__opd_idx]
+        cunit_opd = u.Unit(self.wcs.wcs.cunit[self.__opd_idx])
         naxis_opd = self.shape[0]
 
         if apodization_function is None:
@@ -274,12 +342,12 @@ class FTSData(NDDataArray):
 
         # Build new wcs
         wcs = deepcopy(self.wcs)
-        wcs.wcs.ctype[self._opd_idx] = "FREQ"
-        wcs.wcs.cunit[self._opd_idx] = "Hz"
+        wcs.wcs.ctype[self.__opd_idx] = "FREQ"
+        wcs.wcs.cunit[self.__opd_idx] = "Hz"
         # (cst.c / (cdelt_opd * cunit_opd) / (output_shape-1)).to(u.Hz).value give the 1/2L resolution, but fails in the tests
-        wcs.wcs.cdelt[self._opd_idx] = (cst.c / (cdelt_opd * cunit_opd) / output_shape).to(u.Hz).value
-        wcs.wcs.crpix[self._opd_idx] = naxis_opd
-        wcs.wcs.crval[self._opd_idx] = 0
+        wcs.wcs.cdelt[self.__opd_idx] = (cst.c / (cdelt_opd * cunit_opd) / output_shape).to(u.Hz).value
+        wcs.wcs.crpix[self.__opd_idx] = naxis_opd
+        wcs.wcs.crval[self.__opd_idx] = 0
 
         # TODO: Estimate uncertainty/hits
         output = FTSData(spectra, meta=self.meta, wcs=wcs)
@@ -362,13 +430,13 @@ class FTSData(NDDataArray):
                 import matplotlib.pyplot as plt
 
                 fig, axes = plt.subplots(ncols=4)
-                (freq,) = cube.wcs.sub([self._opd_idx + 1]).all_pix2world(np.arange(cube.shape[0]), 0)
+                (freq,) = cube.wcs.sub([self.__opd_idx + 1]).all_pix2world(np.arange(cube.shape[0]), 0)
                 axes[1].plot(freq, cube.data[:, :, 0])
                 axes[2].plot(freq, _phase[:, :, 0])
 
             if medfilt_size is not None:
                 # Median filtering of the phases
-                _phase = medfilt(_phase, kernel_size=(medfilt_size, 1, 1))
+                _phase = medfilt(_phase, kernel_size=(medfilt_size, *(1,) * (len(itg.shape) - 1)))
 
             if deg is not None:
 
@@ -414,11 +482,11 @@ zero and Nyquist frequency.
                 np.exp(-1j * np.fft.fftshift(np.concatenate([-phase[:0:-1], phase]), axes=0)), axis=0
             )
 
-            # Apodization of the PCF
+            # Apodization of the PCF along the first axis
             phase_correction_function = (
-                np.fft.fftshift(phase_correction_function, axes=0)
-                * pcf_apodization(phase_correction_function.shape[0])[:, np.newaxis, np.newaxis]
-            )
+                np.fft.fftshift(phase_correction_function, axes=0).T
+                * pcf_apodization(phase_correction_function.shape[0])
+            ).T
 
             if plot:
                 (x,) = itg.wcs.sub([3]).all_pix2world(np.arange(itg.shape[0]), 0)
@@ -475,10 +543,12 @@ zero and Nyquist frequency.
 
         # Convolved the interferograms and hits
         itg = np.ma.array(self.data, mask=self.mask).filled(0)
-        hits = np.ma.array(self.hits, mask=self.mask).filled(0)
-
         corrected_itg = fftconvolve(itg, phase_correction_function, mode="same", axes=0).real
-        corrected_hits = fftconvolve(hits, phase_correction_function, mode="same", axes=0).real
+
+        corrected_hits = None
+        if self.hits is not None:
+            hits = np.ma.array(self.hits, mask=self.mask).filled(0)
+            corrected_hits = fftconvolve(hits, phase_correction_function, mode="same", axes=0).real
 
         corrected = FTSData(corrected_itg, wcs=self.wcs, hits=corrected_hits)
 
@@ -614,6 +684,54 @@ zero and Nyquist frequency.
         hdulist = fits.HDUList(hdus)
 
         return hdulist
+
+    @classmethod
+    def from_array(cls, opd, data, hits=None, mask=None, **kwargs):
+        """Construct FTS data from arrays.
+
+        Parameters
+        ----------
+        opd : array_like or Quantity (M,)
+            the optical path difference, by default 'mm'
+        data : array_like (M, *)
+            the corresponding data, first dimension must match opd
+        hits : array_like, optionnal
+            the corresponding hits
+        mask : array_like, optionnal
+            the corresponding mask
+
+        Returns
+        -------
+        data : FTSData
+            the corresponding FTSData objects
+        """
+
+        naxis = len(data.shape)
+        wcs = WCS(naxis=naxis)
+
+        if not isinstance(opd, u.Quantity):
+            opd = u.Quantity(opd, "mm")
+
+        zpd_idx = np.argmin(np.abs(opd))
+
+        if opd[zpd_idx] != 0:
+            print("Shifting opd by {} for 0".format(opd[zpd_idx]))
+            opd -= opd[zpd_idx]
+
+        dpd = np.diff(opd)
+        assert np.all(np.median(dpd) - dpd == 0), "Problem on opd differences"
+
+        wcs.wcs.ctype[naxis - 1] = "OPD"
+        wcs.wcs.cunit[naxis - 1] = opd.unit
+
+        wcs.wcs.crpix[naxis - 1] = zpd_idx + 1
+        wcs.wcs.crval[naxis - 1] = opd[zpd_idx].value
+        wcs.wcs.cdelt[naxis - 1] = np.median(dpd).value
+
+        if mask is None:
+            mask = False
+
+        return cls(data, wcs=wcs, hits=hits, mask=mask | np.isnan(data), **kwargs)
 
 
 def fits_ftsdata_writer(
