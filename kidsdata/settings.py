b4d5885a5d969b3f586d8965b121cbe88a691ad4
Grégoire Duvauchelle <gregoire.duvauchelle@lam.fr>
2021-02-26 11:32:06 +0100

    Remove DB_DIR, use current directory for sqlite by default

--- kidsdata/settings.py
+++ kidsdata/settings.py
@@ -2,8 +2,7 @@ from pathlib import Path
 from decouple import config
 
 # only used in default session of database.api
-DB_DIR = config("DB_DIR", default=".", cast=Path)
-DB_URI = config("DB_URI", default="sqlite:///" + str(DB_DIR.resolve() / "kids_data.db"))
+DB_URI = config("DB_URI", default="sqlite:///" + str(Path(".").resolve() / "kids_data.db"))
 BASE_DIRS = [Path(directory) for directory in config("DATA_DIR", default="/data/CONCERTO/InLab").split(";")]
 
 # other settings
