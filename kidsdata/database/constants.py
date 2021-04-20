14a23f72b39b581a7479ed93e0c55978320f2578
Alexandre Beelen <alexandre.beelen@lam.fr>
2021-04-20 12:16:49 +0000

    fix(re) more permisive re for astro scans

--- kidsdata/database/constants.py
+++ kidsdata/database/constants.py
@@ -7,7 +7,7 @@ PATH_MAX = 4096  # /* # chars in a path name including nul */
 RE_DIR = re.compile(r"X(\d*)_(\d{4,4})_(\d{2,2})_(\d{2,2})$")
 
 # Regular scans : X20201110_0208_S1192_Crab_ITERATIVERASTER
-RE_ASTRO = re.compile(r"^X(\d{8,8})_(\d{4,4})_S(\d{4,4})_([\w|\+]*)_(\w*)$")
+RE_ASTRO = re.compile(r"^X(\d{8,8})_(\d{4,4})_S(\d*)_([\w|\+]*)_(\w*)$")
 
 # Extra files : X_2020_10_22_12h53m20_AA_man :
 RE_MANUAL = re.compile(r"^X_(\d{4,4})_(\d{2,2})_(\d{2,2})_(\d{2,2})h(\d{2,2})m(\d{2,2})_AA_man$")
