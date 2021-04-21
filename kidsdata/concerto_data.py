bc2263f141c7c91836f1152e1772e764fa0eef48
Alexandre Beelen <alexandre.beelen@lam.fr>
2021-04-21 22:19:18 +0000

    fix(ConcertoData) proper time scale for obstime

--- kidsdata/concerto_data.py
+++ kidsdata/concerto_data.py
@@ -1,9 +1,13 @@
 from pathlib import Path
 from autologging import logged
 
+import astropy.units as u
+from astropy.time import Time
+
 from .kiss_continuum import KissContinuum
 from .kiss_spectroscopy import KissSpectroscopy
 from .telescope_positions import MBFitsPositions
+from .kiss_rawdata import KissRawData
 
 
 @logged
@@ -23,6 +27,13 @@ class ConcertoData(KissContinuum, KissSpectroscopy):
     def read_data(self, *args, **kwargs):
 
         super().read_data(*args, **kwargs)
+
+        # ObsTime are not in UTC (the default) but in GPS  !!
+        obstime = Time((self.obstime + 19 * u.s).mjd, format="mjd", scale="tai")
+
+        self._KidsRawData__dataSc["obstime"] = obstime
+        self.obstime = obstime
+        KissRawData.u_obstime.fget.cache_clear()
 
         if self.meta["MBFITS"] is not None and Path(self.meta["MBFITS"]).exists():
             self.telescope_positions = MBFitsPositions(self.meta["MBFITS"], "LONGOFF_LATOFF")
